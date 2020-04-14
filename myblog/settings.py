e9db86a603e6043366e5aaf25496aa7b618854ec
Yaosirrr <yrj0398@sina.com>
2020-04-14 18:58:27 +0800

    修改settings文件，添加部署网站

--- myblog/settings.py
+++ myblog/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'qtatsop2szax-s_y_u_(ts^eww89hjvx^mel_xvfhcqbb(zs09'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['yaosir.pythonanywhere.com']
 
 
 # Application definition
@@ -122,6 +122,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR,'static')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR,'static')
 ]
